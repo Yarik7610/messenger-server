0b588df7fd47a793af8cc30ebc2e63916cfada14
Yaroslav <arikarik782@gmail.com>
2023-12-13 15:01:08 +0300

    test

--- index.js
+++ index.js
@@ -77,11 +77,3 @@ const start = async () => {
   }
 };
 start();
-
-// const server = createServer(app);
-// const io = new Server(server, {
-//   cors: {
-//     origin: "http://localhost:5173",
-//     methods: ["GET", "POST"],
-//   },
-// });
